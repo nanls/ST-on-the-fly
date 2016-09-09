3057fc90caecd0e17164c472db3f823296b8be50
Athénaïs Vaginay <vaginay.athenais@gmail.com>
2016-09-09 19:38:55 +0200

    Ignore warning when creating .tpr

--- code/minimisation.sh
+++ code/minimisation.sh
@@ -7,6 +7,14 @@ mkdir $res_dir
 
 #----------------
 #Get .tpr file, a portable binary run input file containing the starting structure (.pdb) of the simulation, the molecular topology (.top) and all the simulation parameters (.tpr):
-gmx grompp -f ../data/mini2.mdp -c ../data/ala10_md000.pdb -p ../data/ala10.top -po ../data/grompp_mdout.mdp -o ../data/minimi.tpr
+
+#To ignore these warnings : 
+#"
+#Warning: atom name 1 in ala10.top and ala10_md000.pdb does not match (CH3 - HH31)
+#Warning: atom name 2 in ala10.top and ala10_md000.pdb does not match (HH31 - CH3)
+#If you are sure all warnings are harmless, use the -maxwarn option.
+#"                                                  --------
+#XXX TODO : should be ignored?
+gmx grompp -f ../data/mini2.mdp -c ../data/ala10_md000.pdb -p ../data/ala10.top -po ../data/grompp_mdout.mdp -o ../data/minimi.tpr -maxwarn 1
 
 
