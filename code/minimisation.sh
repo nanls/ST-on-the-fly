c4e8e569f954e85372a5887082ea1c88872d880c
Athénaïs Vaginay <vaginay.athenais@gmail.com>
2016-09-09 19:37:05 +0200

    Create .tpr file for minimization

--- code/minimisation.sh
+++ code/minimisation.sh
@@ -5,4 +5,8 @@
 res_dir=../results/minimization-$(date '+%Y-%b-%d-%Hh-%Mmin')
 mkdir $res_dir
 
+#----------------
+#Get .tpr file, a portable binary run input file containing the starting structure (.pdb) of the simulation, the molecular topology (.top) and all the simulation parameters (.tpr):
+gmx grompp -f ../data/mini2.mdp -c ../data/ala10_md000.pdb -p ../data/ala10.top -po ../data/grompp_mdout.mdp -o ../data/minimi.tpr
+
 
