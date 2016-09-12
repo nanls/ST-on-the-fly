924c814915161b97dfbd44a36834b6f62f636c1a
Athenais Vaginay <vaginay.athenais@gmail.com>
2016-09-12 13:31:21 +0200

    Run minimisation bash script

--- code/minimisation.py
+++ code/minimisation.py
@@ -4,3 +4,14 @@
 """
 Run minimisation using gromax
 """
+
+if __name__ == "__main__":
+        
+    import subprocess
+    import shlex
+
+    cmd = 'bash ./minimisation.sh'
+    p = subprocess.Popen(shlex.split(cmd))
+    output = p.communicate()
+    print (output)
+
