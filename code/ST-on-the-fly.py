a1f22b447d1528609f74bb91d50bb8dbb322fa5d
Athenais Vaginay <vaginay.athenais@gmail.com>
2016-09-18 15:51:49 +0200

    DS

--- code/ST-on-the-fly.py
+++ code/ST-on-the-fly.py
@@ -71,6 +71,23 @@ class MolecularDynamics(object):
 
     @logger.log_decorator
     def __init__(self, mdp_filename, gro_filename, top_filename, out_path, out_name, maxwarn, **kwargs):
+        """ Molecular Dynamics constructor 
+
+        Arguments : 
+        -----------
+        mdp_filename : string, private 
+            Path to the mdp file 
+        gro_filename : string, private 
+            Path to the gro file
+        top_filename : string, private 
+            Path to the top file
+        out_path : string, private 
+            Path where to store outputs 
+        out_name : string, private 
+            Templace for the outputs
+        maxwarn : int, private
+            Number of warnings allowed for GROMACS grompp function
+        """
         print ('je suis dans le constructor de MolecularDynamics')
         super(MolecularDynamics, self).__init__(**kwargs)
         self._mdp_filename = mdp_filename
@@ -83,21 +100,63 @@ class MolecularDynamics(object):
 
     @property 
     def mdp_filename(self):
+        """Getter of mdp filename
+
+        Return : 
+        --------
+        mdp_filename : string
+            Path to the mdp file
+        """
         return self._mdp_filename
     @property 
     def gro_filename(self):
+        """Getter of gro filename
+
+        Return : 
+        --------
+        gro_filename : string
+            Path to the gro file
+        """
         return self._gro_filename
     @property 
     def top_filename(self):
+        """Getter of top filename
+
+        Return : 
+        --------
+        top_filename : string
+            Path to the top file
+        """
         return self._top_filename
     @property 
     def out_path(self):
+        """Getter of out path 
+
+        Return : 
+        --------
+        out_path : string
+            Path where to store outputs
+        """
         return self._out_path
     @property 
     def out_name(self):
+        """Getter of out_name
+
+        Return : 
+        --------
+        out_name : string
+            Template name for the outputs 
+        """
         return self._out_name
     @property 
     def maxwarn(self):
+        """Getter of maxwarn
+
+        Return : 
+        --------
+        maxwarn : int
+            Number of warning allowed for GROMACS grompp function
+        """
         return self._maxwarn
 
     @logger.log_decorator
